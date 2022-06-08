ada9f5ce09ea7d2e2d6de4d31352772a5b07eb1b
Aleksey Kurylev <alekseykurylev@gmail.com>
2022-06-08 20:40:20 +0300

    Модуляция

---
+++ js/util.js
@@ -0,0 +1,15 @@
+const getRandomPositiveInteger = function(a, b) {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+const getRandomPositiveFloat = function(a, b, digits = 1) {
+  const lower = Math.min(Math.abs(a), Math.abs(b));
+  const upper = Math.max(Math.abs(a), Math.abs(b));
+  const result = Math.random() * (upper - lower) + lower;
+  return +result.toFixed(digits);
+};
+
+export {getRandomPositiveFloat, getRandomPositiveInteger}
