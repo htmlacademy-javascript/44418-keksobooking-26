6a344d5e85d3d647ec95c67f9e58da0c7a6c8d68
Keks <keks@htmlacademy.ru>
2022-07-18 14:32:44 +0300

    :heavy_check_mark: Сборка #12

--- 12/js/form-file.js
+++ 12/js/form-file.js
@@ -1,9 +1,9 @@
 import {form} from './form.js';
 
-const fileAvatar = form.querySelector('#avatar')
-const avatarPreview = form.querySelector('.ad-form-header__preview > img')
-const filePhoto = form.querySelector('#images')
-const photoPreview = form.querySelector('.ad-form__photo > img')
+const fileAvatar = form.querySelector('#avatar');
+const avatarPreview = form.querySelector('.ad-form-header__preview > img');
+const filePhoto = form.querySelector('#images');
+const photoPreview = form.querySelector('.ad-form__photo > img');
 
 const FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
 
@@ -11,14 +11,12 @@ const addPreview = function(evt, elm) {
   const file = evt.target.files[0];
   const fileName = file.name.toLowerCase();
 
-  const matches = FILE_TYPES.some((it) => {
-    return fileName.endsWith(it);
-  });
+  const matches = FILE_TYPES.some((it) => fileName.endsWith(it));
 
   if (matches) {
     elm.src = URL.createObjectURL(file);
   }
-}
+};
 
 fileAvatar.addEventListener('change', (evt) => {
   addPreview(evt, avatarPreview);
